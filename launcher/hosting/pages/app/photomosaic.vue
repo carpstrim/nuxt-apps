948adbe18cda44d7603490f9a6406ecbb1a70073
国政秀太郎 <knms@Deneb.local>
2019-05-10 01:22:51 +0900

    1st launch but not subimage too downsize

--- launcher/hosting/pages/app/photomosaic.vue
+++ launcher/hosting/pages/app/photomosaic.vue
@@ -6,11 +6,18 @@
     </div>
     <v-container>
       <v-layout wrap>
-        <v-flex xs12 sm6 class="img-box">
+        <v-flex
+          xs12
+          sm6
+          class="img-box"
+        >
           <div class="img-box-inside">
-            <div class="box-text" v-if="!baseUrl">
+            <div
+              class="box-text"
+              v-if="!baseUrl"
+            >
               <upload-btn
-                :fileChangedCallback="uploadFile('baseUrl','single')"
+                :fileChangedCallback="uploadFile('baseUrl',true)"
                 color="primary"
                 title="1.Upload Base-Image"
               ></upload-btn>
@@ -24,31 +31,61 @@
             ></v-img>
           </div>
         </v-flex>
-        <v-flex xs12 sm6 class="img-box">
+        <v-flex
+          xs12
+          sm6
+          class="img-box"
+        >
           <div class="img-box-inside">
-            <div class="box-text">
+            <div
+              class="box-text"
+              v-if="!pmUrl"
+            >
               <v-btn
                 color="info"
                 :disabled="!subUrls.length>0 || !baseUrl"
                 @click="genPhotomosaic"
+                :loading="loadPM"
               >3.Generate Photomosaic</v-btn>
             </div>
+            <v-img
+              :src="pmUrl"
+              v-else
+              @click.right="pmUrl=``"
+              class="base-img"
+              aspect-ratio="1"
+            ></v-img>
+
           </div>
         </v-flex>
-        <v-flex xs12 class="img-box">
+        <v-flex
+          xs12
+          class="img-box"
+        >
           <upload-btn
-            :fileChangedCallback="uploadFile('subUrls','multi')"
+            :fileChangedCallback="uploadFile('subUrls')"
             color="info"
             title="2.Upload Sub-Image"
             multiple
           ></upload-btn>
         </v-flex>
-        <v-flex xs12 class="img-box">{{subUrls.length}}枚 (最大 {{limitSubImg}}枚まで可能)</v-flex>
+        <v-flex
+          xs12
+          class="img-box"
+        >{{subUrls.length}}枚 (最大 {{limitSubImg}}枚まで可能)</v-flex>
       </v-layout>
       <v-layout wrap>
-        <v-flex xs2 v-for="(subUrl,i) in subUrls" :key="i">
+        <v-flex
+          xs2
+          v-for="(subUrl,i) in subUrls"
+          :key="i"
+        >
           <v-card class="sub-img">
-            <v-img :src="subUrl" aspect-ratio="1" @click.right="subUrls.splice(i,1)"/>
+            <v-img
+              :src="subUrl"
+              aspect-ratio="1"
+              @click.right="subUrls.splice(i,1)"
+            />
           </v-card>
         </v-flex>
       </v-layout>
@@ -57,17 +94,28 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
       subUrls: [],
       baseUrl: "",
-      pmUrl: ""
+      pmUrl: "",
+      loadPM: false,
+      limitPhotos: 0,
+      baseWidth: 0,
+      baseHeight: 0
     };
   },
   computed: {
     limitSubImg() {
-      return this.subUrls.length + 10;
+      const rate = Math.max(
+        2400 / Math.min(this.baseWidth, this.baseHeight),
+        1
+      );
+      const fixH = this.baseHeight * rate;
+      const fixW = this.baseWidth * rate;
+      return Math.ceil((fixH / 50) * (fixW / 50) * 1.1) || 0;
     }
   },
   methods: {
@@ -75,6 +123,7 @@ export default {
       window.alert("hoge");
     },
     async genPhotomosaic() {
+      this.loadPM = true;
       const url =
         "https://us-central1-hands-on-campus-apps.cloudfunctions.net/photomosaic";
       const data = {
@@ -82,14 +131,16 @@ export default {
         url_base: this.baseUrl
       };
 
-      this.pmUrl = await this.$axios.post(url, data, {
-        headers: {
-          Authorization: "hogehoge"
-        }
-      });
-      console.log(this.pmUrl);
+      this.pmUrl = await this.$axios
+        .$post(url, data, {
+          headers: {
+            Authorization: "hogehoge"
+          }
+        })
+        .catch(window.alert);
+      this.loadPM = false;
     },
-    uploadFile(key) {
+    uploadFile(key, isBase) {
       return async files => {
         console.log({ files, len: files.length });
         if (!files) return;
@@ -101,9 +152,18 @@ export default {
         for (let i = 0; i < files.length; i++) {
           if (files[i].type.indexOf("image") === -1) continue;
           const reader = new FileReader();
-          reader.onload = e => {
-            if (!isSingle) this[key].push(e.target.result);
-            if (isSingle) this[key] = e.target.result;
+          const image = new Image();
+          reader.onload = ({ target: { result } }) => {
+            image.src = result;
+            image.onload = () => {
+              1;
+              if (!isBase) return;
+              console.log({ w: image.naturalWidth, h: image.naturalHeight });
+              this.baseWidth = image.naturalWidth;
+              this.baseHeight = image.naturalHeight;
+            };
+            if (!isSingle) this[key].push(result);
+            if (isSingle) this[key] = result;
           };
           reader.readAsDataURL(files[i]);
         }
