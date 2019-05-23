6a2f3c7d6927ccc76b9c2dafa260657753070712
kunimasa0903 <str.kunimasa@hands-on-campus.com>
2019-05-23 17:19:48 +0900

    refresh

---
+++ launcher/hosting/pages/sandbox.vue
@@ -0,0 +1,13 @@
+<template>
+  <datetime-picker v-model="datetime" range/>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      datetime: ""
+    };
+  }
+};
+</script>
--- launcher/hosting/plugins/components.js
+++ launcher/hosting/plugins/components.js
@@ -3,11 +3,14 @@ import Vue from 'vue'
 import MaterialCard from '@/components/MaterialCard'
 import HelperOffset from '@/components/Offset'
 import UploadBtn from '@/components/UploadBtn'
+import DatetimePicker from 'vue-ctk-date-time-picker'
+import 'vue-ctk-date-time-picker/dist/vue-ctk-date-time-picker.css';
 
 Vue.mixin({
   components: {
     MaterialCard,
     HelperOffset,
-    UploadBtn
+    UploadBtn,
+    DatetimePicker
   }
 })
