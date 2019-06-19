f7a27901b9e31ee672f5f472ba3ed267a6939bae
国政秀太郎 <knms@deneb.lan>
2019-06-20 00:49:12 +0900

    merged

--- launcher/hosting/pages/app/qrcode-reader.vue
+++ launcher/hosting/pages/app/qrcode-reader.vue
@@ -1,22 +1,53 @@
 <template>
-  <v-container fluid grid-list-lg class="container">
-    <v-layout row wrap justify-center>
-      <div id="e1" style="max-width: 500px; margin: auto;">
+  <v-container
+    fluid
+    grid-list-lg
+    class="container"
+  >
+    <v-layout
+      row
+      wrap
+      justify-center
+    >
+      <div
+        id="e1"
+        style="max-width: 500px; margin: auto;"
+      >
         <v-flex xs12>
           <h1 class="title">QRCode Reader</h1>
         </v-flex>
       </div>
-      <v-flex xs12 justify-center>
-        <v-btn fab dark middle color="primary" @click="isCameraOpen=!isCameraOpen">
+      <v-flex
+        xs12
+        justify-center
+      >
+        <v-btn
+          fab
+          dark
+          middle
+          color="primary"
+          @click="isCameraOpen=!isCameraOpen"
+        >
           <v-icon>camera_enhance</v-icon>
         </v-btn>
         <p></p>
 
-        <p v-show="doneQR&&isCameraOpen" class="suc">QRコードの読み取りに成功しました。しばらくお待ち下さい。</p>
-        <v-progress-circular indeterminate color="primary" v-show="doneQR&&isCameraOpen"></v-progress-circular>
+        <p
+          v-show="doneQR&&isCameraOpen"
+          class="suc"
+        >QRコードの読み取りに成功しました。しばらくお待ち下さい。</p>
+        <v-progress-circular
+          indeterminate
+          color="primary"
+          v-show="doneQR&&isCameraOpen"
+        ></v-progress-circular>
       </v-flex>
       <v-flex xs12>
-        <video :width="width" :height="height" autoplay/>
+        <video
+          :width="width"
+          :height="height"
+          autoplay
+        />
       </v-flex>
       <v-flex xs12>{{readed}}</v-flex>
     </v-layout>
@@ -25,7 +56,6 @@
 
 <script>
 import jsQR from "jsQR";
-
 export default {
   data() {
     return {
